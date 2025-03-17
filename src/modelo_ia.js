59036e168b452b2744def7469cd1eaaaa14d3534
alozjim955 <alozjim955@fp.iesromervargas.com>
2025-03-17 21:48:41 +0100

    Prototipo
    Prueba de la API del modelo IA

---
+++ src/modelo_ia.js
@@ -0,0 +1,17 @@
+import OpenAI from "openai";
+import dotenv from "dotenv";
+dotenv.config();
+
+const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+const completion = await client.chat.completion.create({
+    model: "gpt-4o",
+    messages: [
+        {
+            role: "user",
+            content: "Escribe una frase graciosa de 10 palabras.",
+        },
+    ],
+});
+
+console.log(completion.choices[0].message.content);
